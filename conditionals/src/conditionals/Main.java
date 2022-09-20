589d86b324f73bb8a7cef80f4a04d8bc9b7e3ffc
Umut Sahin <umutshn28@gmail.com>
2022-09-20 16:48:40 +0300

    Proje Dosyalari Eklendi.

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,18 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 03;
+
+		if (sayi < 18) {
+			System.out.println("Sayı 18'den küçüktür.");
+		} else if (sayi == 18) {
+			System.out.println("Sayi 18'e eşittir.");
+		} else {
+			System.out.println("Sayı 18'den büyüktür.");
+		}
+
+	}
+
+}
---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 18.03;
+		sayi = -18;
+		
+		char karakter = 'B';
+		
+		boolean dogruMu = true;
+		
+		System.out.println(dogruMu);
+		System.out.println(karakter);
+		System.out.println(sayi);
+		
+	}
+
+}
