fbdaa7e1cbb066980d7a3e718de487c663349406
Umut Sahin <umutshn28@gmail.com>
2022-09-28 22:37:26 +0300

    2. Hafta Proje Dosyaları Eklendi.

--- classesWithAttributes/src/classesWithAttributes/Main.java
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -3,16 +3,19 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// Product product = new Product(1, "Laptop", "Monster", 5000, 2, "Siyah");
+
 		Product product = new Product();
-		product.name="Laptop";
-		product.description="Monster";
-		product.price=5000;
-		product.stockAmount=2;
-		
-		
-		ProductManager productManager=new ProductManager();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Monster");
+		product.setPrice(5000);
+		product.setStockAmount(2);
+
+		ProductManager productManager = new ProductManager();
 		productManager.Add(product);
-		
+		System.out.println(product.getKod());
 
 	}
 
